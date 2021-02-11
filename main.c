87ffc13d899da31ff48217c7c2f38817d4f8d397
pinknemo452 <mishanay38@gmail.com>
2021-02-12 00:46:59 +0300

    fix #15

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 #define gap 10000
 
 int main() { 
-	unsigned int r;
-	mySrand(time(NULL),&r);
+	unsigned int seed;
+	mySrand(time(NULL),&seed);
 	unsigned int statistic[RAND_MOD/gap + 1] = { 0 };
 	/*printf("you can enter seed ");
 	int seed;
 	scanf("%d", &seed);
 	mySrand(seed);*/
 	for (int i = 0; i < 500000; ++i) {
-		statistic[myRand(&r) / gap]++;
+		statistic[myRand(&seed) / gap]++;
 	}
 	for (int i = 0; i < 10; ++i) {
 		printf("[%d .. %d): %d\n", i*gap, (i+1)*gap-1, statistic[i]);
--- my_lab.h
+++ my_lab.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef MY_LAB_H
+#define MY_LAB_H
 #define RAND_MOD 100000
 void mySrand(unsigned int x, unsigned int* r);
 unsigned int myRand(unsigned int* r);
+#endif
